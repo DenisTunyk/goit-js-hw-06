27a7d32a566f24935d702685a0e1f32c7652c48c
DenisTunyk <tundenis43@gmail.com>
2022-10-20 18:05:13 +0300

    commity

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +8,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// for (let i = 0; i < 10; i = +1){
+//   console.log(i);
+// }
+
+
+
+const ulElem = document.querySelector("#ingredients");
+
+// const prod = ingredients.map(item => `<li class="item">${item}</li>`).join("");
+// ulElem.insertAdjacentHTML('beforeend', prod);
+
+
+const arr = [];
+for (let i = 0; i < ingredients.length; i += 1){
+  console.log(ingredients[i]);
+  const li = document.createElement('li');
+  li.classList.add("item");
+  li.textContent = ingredients[i];
+  arr.push(li);
+}
+ulElem.append(...arr);
+ 
