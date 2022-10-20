952bfde4a565846a134647a9e5f1ea79971ada8a
DenisTunyk <tundenis43@gmail.com>
2022-10-20 17:05:56 +0300

    commit

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,11 @@
 "use strict";
 
 const categories = document.querySelector("#categories");
-console.dir(categories);
+
+console.log("Number of categories:", categories.children.length);
+console.log("");
+[...categories.children].forEach(elem => {
+    console.log("Category:", elem.firstElementChild.textContent);
+    console.log("Elements:", elem.lastElementChild.children.length);
+    console.log("");
+});
