1e28e69174dbbc216c48184534cdde69e17f6514
DenisTunyk <tundenis43@gmail.com>
2022-10-21 15:08:04 +0300

    Home

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const ref = {
+    counter: document.querySelector("#counter"),
+    valueEl: document.querySelector("#value"),
+}
+
+let counterValue = 0;
+
+ref.counter.firstElementChild.addEventListener('click', () => {
+    counterValue -= 1;
+    ref.valueEl.textContent = counterValue;
+});
+
+ref.counter.lastElementChild.addEventListener('click', () => {
+    counterValue += 1;
+    ref.valueEl.textContent = counterValue;
+});
+
+//ref.valueEl.innerHTML = counterValue;
+
+
+
