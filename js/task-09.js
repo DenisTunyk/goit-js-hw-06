b4cc799bab04a9818b8afd02a1d1de111834185a
DenisTunyk <tundenis43@gmail.com>
2022-10-24 13:46:38 +0300

    upate task 09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
+const refs = {
+  button: document.querySelector("button.change-color"),
+  span: document.querySelector("span.color"),
+}
+
+refs.button.addEventListener("click", onButtonClick);
+
+function onButtonClick(e) {
+
+  const backColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = backColor;
+  refs.span.textContent = backColor;
+
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
