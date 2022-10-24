dccd8b5e85da59aa447026cb84fd1c76c64f20d9
DenisTunyk <tundenis43@gmail.com>
2022-10-24 13:36:39 +0300

    example 8 full

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const formElements = e.currentTarget.elements;
+    const email = formElements.email.value;
+    const pass = formElements.password.value;
+
+    if ((!email) || (!pass)) {
+        alert("Всі поля повинні бути заповнені");
+    } else {      
+        const formD = {
+            email,
+            pass,
+        }
+
+        console.log(formD);
+        form.reset();
+
+        // const formData = new FormData(e.currentTarget);
+        // console.log(formData);
+        // formData.forEach((value, name) => {
+        //     console.log(name);
+        //     console.log(value);
+        // })
+    }
+}
